5872661d02518b14d7c9c282e755a0b56154ae4a
JDeegan20 <20085768@mail.wit.ie>
2023-03-02 13:45:27 +0000

    Event Handling

--- app/src/main/java/org/wit/recordshop/RecordActivity.kt
+++ app/src/main/java/org/wit/recordshop/RecordActivity.kt
@@ -1,16 +1,31 @@
 package org.wit.recordshop
 
+import org.wit.recordshop.databinding.ActivityRecordBinding
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import timber.log.Timber
 import timber.log.Timber.i
 
 class RecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started..")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started..")
+
+        
+        binding = ActivityRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
+        }
     }
 }
 
