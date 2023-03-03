84371310a07ec370ded3c29dfbf6fd9676457dd1
JDeegan20 <20085768@mail.wit.ie>
2023-03-03 14:31:24 +0000

    Event Handling

--- app/src/main/java/org/wit/recordshop/activities/RecordListActivity.kt
+++ app/src/main/java/org/wit/recordshop/activities/RecordListActivity.kt
@@ -1,6 +1,7 @@
 package org.wit.recordshop.activities
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,12 +9,14 @@ import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.record.adapters.RecordAdapter
+import org.wit.record.adapters.RecordListener
 import org.wit.recordshop.R
 import org.wit.recordshop.databinding.ActivityRecordListBinding
 import org.wit.recordshop.main.MainApp
+import org.wit.recordshop.models.RecordModel
 
 
-class RecordListActivity : AppCompatActivity() {
+class RecordListActivity : AppCompatActivity(), RecordListener {
 
     lateinit var app: MainApp
     private lateinit var binding: ActivityRecordListBinding
@@ -25,13 +28,11 @@ class RecordListActivity : AppCompatActivity() {
         binding.toolbar.title = title
         setSupportActionBar(binding.toolbar)
 
-
-
         app = application as MainApp
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = RecordAdapter(app.records.findAll())
+        binding.recyclerView.adapter = RecordAdapter(app.records.findAll(),this)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -47,8 +48,22 @@ class RecordListActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
     private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                (binding.recyclerView.adapter)?.
+                notifyItemRangeChanged(0,app.records.findAll().size)
+            }
+        }
+
+    override fun onRecordClick(record: RecordModel) {
+        val launcherIntent = Intent(this, RecordActivity::class.java)
+        getClickResult.launch(launcherIntent)
+    }
+
+    private val getClickResult =
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) {
--- app/src/main/java/org/wit/recordshop/adapters/RecordAdapter.kt
+++ app/src/main/java/org/wit/recordshop/adapters/RecordAdapter.kt
@@ -6,18 +6,23 @@ import androidx.recyclerview.widget.RecyclerView
 import org.wit.recordshop.databinding.CardRecordBinding
 import org.wit.recordshop.models.RecordModel
 
-class RecordAdapter constructor(private var records: List<RecordModel>) :
+interface RecordListener {
+    fun onRecordClick(record: RecordModel)
+}
+class RecordAdapter constructor(private var records: List<RecordModel>,
+                                   private val listener: RecordListener) :
     RecyclerView.Adapter<RecordAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         val binding = CardRecordBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
+
         return MainHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
         val record = records[holder.adapterPosition]
-        holder.bind(record)
+        holder.bind(record, listener)
     }
 
     override fun getItemCount(): Int = records.size
@@ -25,10 +30,11 @@ class RecordAdapter constructor(private var records: List<RecordModel>) :
     class MainHolder(private val binding : CardRecordBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(record: RecordModel) {
+        fun bind(record: RecordModel, listener: RecordListener) {
             binding.recordTitle.text = record.title
             binding.description.text = record.description
-            binding.genre.text = record.genre
+            binding.root.setOnClickListener { listener.onRecordClick(record) }
         }
     }
 }
+
