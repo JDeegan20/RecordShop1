30fc7a3a44e1d2b834c9440b82e80fd730036fa0
JDeegan20 <20085768@mail.wit.ie>
2023-04-27 18:49:07 +0100

    Content Layouts, Markers + Cardview

--- app/src/main/java/org/wit/recordshop/activities/RecordMapsActivity.kt
+++ app/src/main/java/org/wit/recordshop/activities/RecordMapsActivity.kt
@@ -1,24 +1,87 @@
-package org.wit.recordshop.activities
+package org.wit.placemark.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+
 import org.wit.recordshop.databinding.ActivityRecordMapsBinding
+import org.wit.recordshop.databinding.ContentRecordMapsBinding
+import org.wit.recordshop.main.MainApp
 
+class RecordMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
-class RecordMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecordMapsBinding
+    private lateinit var contentBinding: ContentRecordMapsBinding
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = application as MainApp
 
         binding = ActivityRecordMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentRecordMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
 
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.records.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+            map.setOnMarkerClickListener(this)
+
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        contentBinding.currentTitle.text = marker.title
+
+        return false
+    }
 }
